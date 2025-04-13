bd581c23a9799e5ca78191a9ae27407224e75a20
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-04-14 02:18:49 +0530

    feat: Replace anchor tag with button for Instagram login

--- client/src/components/LoginButton.tsx
+++ client/src/components/LoginButton.tsx
@@ -6,13 +6,17 @@ export default function LoginButton() {
 
   const authUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
 
+  const handleClick = () => {
+    window.location.href = authUrl;
+  };
+
   return (
-    <a
-      href={authUrl}
-      className="text-xl font-semibold rounded-xl flex gap-8 items-center w-96 px-8 py-4 m-4 cursor-pointer bg-gradient-to-r from-pink-500 to-purple-500 hover:from-pink-600 hover:to-purple-600 transition-colors duration-300 "
+    <button
+      onClick={handleClick}
+      className="text-base sm:text-xl font-semibold rounded-xl shadow-lg flex gap-8 items-center w-72 sm:w-96 px-4 sm:px-8 py-3 sm:py-4 m-4 cursor-pointer bg-gradient-to-r from-pink-600 to-purple-600 hover:from-pink-700 hover:to-purple-700 hover:scale-105 transition-all"
     >
       <FaInstagram size={32} color="white" />
-      Sign in with Instagram
-    </a>
+      Login with Instagram
+    </button>
   );
 }
