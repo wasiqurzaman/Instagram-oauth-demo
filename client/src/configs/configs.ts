0cc5467c5a6072e46765e79e361d7eeec686d132
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-04-15 13:41:18 +0530

    fix: Update redirectUri to correct endpoint for authentication

--- client/src/configs/configs.ts
+++ client/src/configs/configs.ts
@@ -1,5 +1,5 @@
 export const baseUrl = "https://instagram-oauth-demo.onrender.com/api";
 export const redirectUri =
-  "https://instagram-oauth-demo-brown.vercel.app/auth/redirect";
+  "https://instagram-oauth-demo-1.onrender.com/auth/redirect";
 export const clientId = "1236148904897293";
 export const scope = `instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights`;
