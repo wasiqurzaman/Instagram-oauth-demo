88fe508dc07d5755a2738f528bd1b305df57637b
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-04-15 12:51:16 +0530

    chore: Comment out unused server configuration in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
-const __dirname = import.meta.dirname;
+// const __dirname = import.meta.dirname;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "cert/key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "cert/cert.pem")),
-    },
-    port: 5173,
-  },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync(path.resolve(__dirname, "cert/key.pem")),
+  //     cert: fs.readFileSync(path.resolve(__dirname, "cert/cert.pem")),
+  //   },
+  //   port: 5173,
+  // },
 });
