883a2d9e71e07d0cf42b2941af72beb78c3f7a39
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-04-15 08:11:32 +0530

    feat: Refactor AppLayout for improved layout structure and responsiveness

--- client/src/components/AppLayout.tsx
+++ client/src/components/AppLayout.tsx
@@ -3,11 +3,11 @@ import SideNavbar from "./SideNavbar";
 
 export default function AppLayout() {
   return (
-    <div className="flex  bg-gray-100 dark:bg-zinc-950 transition-colors">
-      <div className="h-screen sticky top-0 z-30">
+    <div className="flex flex-col md:flex-row min-h-screen bg-gray-100 dark:bg-zinc-950 transition-colors">
+      <div className="h-screen sticky top-0 left-0">
         <SideNavbar />
       </div>
-      <main className="flex-1 min-h-screen overflow-y-auto max-h-screen p-4">
+      <main className="flex-1 p-4">
         <Outlet />
       </main>
     </div>
